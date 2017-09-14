4d405379a6d9a91a9d949bf3577ea548f1c05022
noruby <noruby>
2017-09-14 11:06:37 +0900

    my third commit

--- Projects_by2017/SemicondoctorBlochEquation/2band_SBE_for_HHG/2band_SBE_HHG.R
+++ Projects_by2017/SemicondoctorBlochEquation/2band_SBE_for_HHG/2band_SBE_HHG.R
@@ -13,18 +13,18 @@ elementary_charge <- 1.60217662 * 10^-19 # Coulombs
 #Parameters of experiment / material properties
 Temperature <- 300 #K
 Fermi_energy <- # Chemical potential
-lattice_constant_a_of_GaSe <- 3.755 *10^-10# m (3.755 ångström)
+lattice_constant_a_of_GaSe <- 3.755 * 2/sqrt(3) *10^-10# m (3.755 ångström)
 
 # Time t
 range_t <- 200 *10^(-15) # fs
-div_num_t <- 1000 # n
+div_num_t <- 1000000 # n
 delta_t <-  range_t/ div_num_t
 t_vector <- seq( -range_t/2, range_t/2, length= (div_num_t+1))
 t_vector2 <- seq( -range_t/2, range_t/2, length= (2*div_num_t+1))
 frequency <- seq(0, 1/delta_t, length= (div_num_t+1))
 
 # External electric field (assuming the gaussian envelope)
-E_amplitude_max <- 0.1* 10^8 # V/m
+E_amplitude_max <- 100* 10^8 # V/m
 envelope_width <- 50* 10^(-15) # 1/e^2 width; 50 fs 
 carrier_frequency <- 33 *10^12 # 33 THz
 carrier_envelope_phase <- 0 # [rad]
@@ -33,14 +33,14 @@ E_vector2 <-  E_amplitude_max * exp(-t_vector2^2/envelope_width^2) *cos(2*pi* ca
 E_spectrum <- fft(E_vector)
 
 # External electric field (step function)
-E_vector[1:(div_num_t+1)] <-  rep(0,div_num_t+1)
-E_vector[101:(div_num_t+1)] <-  rep(E_amplitude_max, div_num_t-99)
-E_vector2[1:(2*div_num_t+1)] <-  rep(0,2*div_num_t+1)
-E_vector2[201:(2*div_num_t+1)] <-  rep(E_amplitude_max, 2*div_num_t-749)
+#E_vector[1:(div_num_t+1)] <-  rep(0,div_num_t+1)
+#E_vector[101:(div_num_t+1)] <-  rep(E_amplitude_max, div_num_t-99)
+#E_vector2[1:(2*div_num_t+1)] <-  rep(0,2*div_num_t+1)
+#E_vector2[201:(2*div_num_t+1)] <-  rep(E_amplitude_max, 2*div_num_t-199)
 
 
 # k normalized by inverse lattice constant 1/a 
-div_num_ka <- 1000 # m
+div_num_ka <- 150 # m
 delta_k <- 2*pi / lattice_constant_a_of_GaSe / div_num_ka 
 ka_vector_redundant <- seq( -pi, pi, length= (div_num_ka+1))
 ka_vector <- ka_vector_redundant[1:div_num_ka]
@@ -70,29 +70,85 @@ dev.off()           
 
 
 #dipole transition matrix
-Rabi_energy_max <- 1.2 *1.60218*10^-19 #1.2eV; converstion from eV to J
-dipole_transition <- Rabi_energy_max / E_amplitude_max
+Rabi_energy_max <- 0.12 *1.60218*10^-19 #1.2eV; converstion from eV to J
+dipole_transition_max <- Rabi_energy_max / E_amplitude_max 
+dipole_transition <- dipole_transition_max * min(energy_diff) / energy_diff 
 
 #relaxation
-relaxation_constant <- 20 *10^(-15) # 7fs 
-
+relaxation_constant <-  50 *10^(-15) # 7fs 
 #dephasing 
 dephasing_constant <- 5 *10^(-15) # 1.1fs 
 
+#parameters of calculation
+Courant_number <- delta_t * E_amplitude_max *  elementary_charge / Plank_constant / delta_k 
+print("Courant number: " )
+print( Courant_number )
+
+w_2 <- rep(0, div_num_ka)
+w_1 <- rep(0, div_num_ka)
+w1 <- rep(0, div_num_ka)
+w2 <- rep(0, div_num_ka)
+ww <- rep(0, div_num_ka)
+
+grad_k0 <- function(E, w0){
+	w1[div_num_ka] <- w0[1]
+	w1[1:(div_num_ka-1)] <- w0[2:div_num_ka]
+	return ((w0-w1) / delta_k)
+}
+
+grad_k1 <- function(E, w0){
+	w_1[1] <- w0[div_num_ka]
+	w_1[2:div_num_ka] <- w0[1:(div_num_ka-1)]
+	w1[div_num_ka] <- w0[1]
+	w1[1:(div_num_ka-1)] <- w0[2:div_num_ka]
+	return ((w1-w_1)/ 2 / delta_k)
+}
+
+grad_k2 <- function(E, w0){
+	w_2[1:2] <- w0[(div_num_ka-1):div_num_ka]
+	w_2[3:div_num_ka] <- w0[1:(div_num_ka-2)]		
+	w_1[1] <- w0[div_num_ka]
+	w_1[2:div_num_ka] <- w0[1:(div_num_ka-1)]
+	w1[div_num_ka] <- w0[1]
+	w1[1:(div_num_ka-1)] <- w0[2:div_num_ka]
+	w2[(div_num_ka-1):div_num_ka] <- w0[1:2]
+	w2[1:(div_num_ka-2)] <- w0[3:div_num_ka]
+
+	w_left <- 2*w1 + 3*w0  - 6*w_1 + w_2		
+	w_right <- - w2 + 6*w1 - 3*w0  - 2*w_1
+	ww <- w_left
+	for(i in 1:div_num_ka){
+		if(w_left[i] < 0)
+		ww[i] <- w_left[i]
+	}
+	return (ww / 6 / delta_k)
+}
+#	ww <- replace( w_right, which(w_right > 0), w_left )  
+
+grad_k3 <- function(E, w0){
+	w_2[1:2] <- w0[(div_num_ka-1):div_num_ka]
+	w_2[3:div_num_ka] <- w0[1:(div_num_ka-2)]		
+	w_1[1] <- w0[div_num_ka]
+	w_1[2:div_num_ka] <- w0[1:(div_num_ka-1)]
+	w1[div_num_ka] <- w0[1]
+	w1[1:(div_num_ka-1)] <- w0[2:div_num_ka]
+	w2[(div_num_ka-1):div_num_ka] <- w0[1:2]
+	w2[1:(div_num_ka-2)] <- w0[3:div_num_ka]
+
+	ww <- (- w2 + 8*w1 - 8*w_1 + w_2	)/ 12 / delta_k
+	return (ww)
+}
+
 fn <- function(E, p, n){
-	dn[1] <- n[div_num_ka]
-	dn[2:div_num_ka] <- n[1:(div_num_ka-1)]
 	nn <- ( 2*Im(E*dipole_transition*p) / Plank_constant
-	+ elementary_charge* E* (n-dn) / delta_k / Plank_constant
+	+ elementary_charge* E* grad_k1(E, n) / Plank_constant
 	- n / relaxation_constant ) * delta_t
 	return (nn)
 }
 
 fp <- function(E, p, nh, ne){
-	dp[1] <- p[div_num_ka]
-	dp[2:div_num_ka] <- p[1:(div_num_ka-1)]	
 	pp <- ( -1i *energy_diff *p / Plank_constant
-	+ elementary_charge* E* (p-dp) / delta_k  / Plank_constant
+	+ elementary_charge* E* grad_k1(E, p) / Plank_constant
 	+ 1i* E* dipole_transition * (1- nh- ne) / Plank_constant
 	- p / dephasing_constant )* delta_t
 	return (pp)
@@ -105,8 +161,6 @@ polarization_kt  <- array(0, dim=c(div_num_ka, (div_num_t+1)))
 number_hole <- rep(0, div_num_ka)
 number_electron <- rep(0, div_num_ka) 
 polarization <- rep(0, div_num_ka)
-dn <- rep(0, div_num_ka)
-dp <- rep(0, div_num_ka)
 
 J_hole <- rep(0, (div_num_t+1))
 J_electron <- rep(0, (div_num_t+1))
@@ -150,8 +204,10 @@ for( j in 1:(div_num_t) ){
 	for(i in 1:(div_num_ka)){
 		J_hole <-  J_hole + J_hole_k[i] * number_hole_kt[i,]
 		J_electron <- J_electron + J_electron_k[i] * number_electron_kt[i,]
-		P <- P + polarization_kt[i,] * dipole_transition	
+		P <- P + polarization_kt[i,] * dipole_transition	[i]
 	}
+
+
 
 P_diff[2:(div_num_t+1)] <- P[1:div_num_t]
 dP_dt <- (P-P_diff)/delta_t
@@ -159,23 +215,34 @@ J <- J_hole + J_electron
 E_HHG <- J +dP_dt
 
 E_HHG_spectrum <- fft(E_HHG)
+J_spectrum <-  fft(J)
+dP_dt_spectrum <- fft(dP_dt)
 
+if(0){
+png("HHG_para.png", width = 500, height = 500)  
+filled.contour(CEP/pi, frequency/ carrier_frequency, -log10(HHG_CEP), ylim=c(0,10), zlim=c(-15,-5),col=rainbow(256),nlevels=(256))
+dev.off() 
+}
 
 warnings()
 
 if(1){
+numt <- round( 2*10^-15 /delta_t )
+print("numt" )
+print(numt)
+nplim=c(-0.7, 0.7)
 library(animation)
 saveGIF({
   ani.options(loop = TRUE)
-  for (i in seq(1,(div_num_t+1), by=25)) {
+  for (i in seq(1,(div_num_t+1), by=numt)) {
     .main = paste(t_vector[i]*10^15,"(fs)", seq="")
-    plot(ka_vector, number_electron_kt[,i], xlim=c(-pi,pi), ylim=c(-1, 1), xlab="BZ", ylab= "numbers", main=.main,col="green", type="l")
+    plot(ka_vector, number_electron_kt[,i], xlim=c(-pi,pi), ylim=nplim, xlab="BZ", ylab= "numbers", main=.main,col="green", type="l")
     par(new=T)
-    plot(ka_vector, number_hole_kt[,i], xlim=c(-pi,pi), ylim=c(-1, 1), xlab="", ylab= "", main=.main, col="blue", type="l")
+    plot(ka_vector, number_hole_kt[,i], xlim=c(-pi,pi), ylim=nplim, xlab="", ylab= "", main=.main, col="blue", type="l")
     par(new=T)    
-    plot(ka_vector, Re(polarization_kt[,i]), xlim=c(-pi,pi), ylim=c(-1, 1), xlab="BZ", ylab= "polarization", main=.main,col="red" , type="l")
+    plot(ka_vector, Re(polarization_kt[,i]), xlim=c(-pi,pi), ylim=nplim, xlab="BZ", ylab= "polarization", main=.main,col="red" , type="l")
   }
-}, movie.name="number_polarization.gif", interval=0.3)
+}, movie.name="number_polarization.gif", interval=0.2)
 }
 
 Jrange <- 3*c( min(Re(J)), max(Re(J)))
@@ -196,10 +263,11 @@ dev.off()       
 
 E_HHGrange <- c( -max(abs(E_HHG)), max(abs(E_HHG)) ) 
 Erange <- c( -max(E_vector), max(E_vector) ) 
+trange <- c(-100, 100)
 png("E.png", width = 700, height = 500)  
-plot(t_vector*10^15, Re(E_HHG), xlab="t (fs)", ylab="Electric field (arb. unit)", ylim=E_HHGrange, axes=FALSE, yaxt="n", col="red", type="l")  
+plot(t_vector*10^15, Re(E_HHG), xlab="t (fs)", ylab="Electric field (arb. unit)", xlim=trange,  ylim=E_HHGrange, axes=FALSE, yaxt="n", col="red", type="l")  
 par(new=T)
-plot(t_vector*10^15, Re(E_vector), xlab="", ylab= "", ylim=Erange, col="black", type="l")  
+plot(t_vector*10^15, Re(E_vector), xlab="", ylab= "", xlim=trange, ylim=Erange, col="black", type="l")  
 #par(new=T)
 #plot(t_vector*10^15, Re(dP_dt), xlab="", ylab= "", ylim=Erange, col="green", type="l")  
 #par(new=T)
@@ -215,11 +283,19 @@ dev.off()       
 #dev.off() 
 
 frange <- c(0, 20)
-Prange <- c(1, 10000000000000)
-png("E_spectrum.png", width = 700, height = 500)  
-plot(frequency/carrier_frequency, abs(E_spectrum)^2/100000000000, xlim=frange, ylim=Prange, log="y", xlab="f/carrier frequency", ylab="power (arb. unit)",col="black", type="l")  
+Erange <- 10^c(max(2*log10(abs(E_spectrum)))-15, max(2*log10(abs(E_spectrum)))+3)
+E_HHGrange <- 10^c(max(2*log10(abs(E_HHG_spectrum)))-13, max(2*log10(abs(E_HHG_spectrum)))+5)
+#Jrange <- 10^c(max(2*log10(abs(J_spectrum)))-13, max(2*log10(abs(J_spectrum)))+5)
+#dP_dtrange <- 10^c(max(2*log10(abs(dP_dt_spectrum)))-13, max(2*log10(abs(dP_dt_spectrum)))+5)
+
+png("E_spectrum.png", width = 700, height = 500) 
+plot(frequency/carrier_frequency, abs(E_spectrum)^2, xlim=frange, ylim=Erange, log="y", xlab="f/carrier frequency", ylab="power (arb. unit)",col="black", type="l")  
 par(new=T)
-plot(frequency/carrier_frequency, abs(E_HHG_spectrum)^2/1000, xlim=frange, ylim=Prange, log="y", axes=FALSE, yaxt="n",xlab="", ylab= "", col="red", type="l")  
+plot(frequency/carrier_frequency, abs(E_HHG_spectrum)^2, xlim=frange, ylim=E_HHGrange, log="y", axes=FALSE, yaxt="n",xlab="", ylab= "", col="red", type="l")  
+par(new=T)
+plot(frequency/carrier_frequency, abs(J_spectrum)^2, xlim=frange, ylim=E_HHGrange, log="y", axes=FALSE, yaxt="n",xlab="", ylab= "", col="green", type="l")  
+par(new=T)
+plot(frequency/carrier_frequency, abs(dP_dt_spectrum)^2, xlim=frange, ylim=E_HHGrange, log="y", axes=FALSE, yaxt="n",xlab="", ylab= "", col="blue", type="l")  
 abline(v=0:20, col='black', lty="dotted")
 dev.off()     
 
