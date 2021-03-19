386e6881ac7db05fd8dfd3a044e05ef59475f9a8
noruby <japanista.nobuyoshi@gmail.com>
2021-03-19 11:18:57 +0900

    Delete Projects_by2017/PCA_university_ranking directory

--- Projects_by2017/PCA_university_ranking/university_ranking_PCA.R
+++
@@ -1,6 +0,0 @@
-data <- read.csv("./university_ranking.csv", head=T, row.name=1)
-pca <-  prcomp(data, scale=T)
-png("./university_ranking_PCA.png", width = 1500, height = 1500)  
-biplot(pca) 
-dev.off() 
-
